aea9465f577b994408fdcdbf371cbd00e2f72458
smadankulov <salam.kg@gmail.com>
2025-02-27 17:05:15 +0600

    spring boot task manager app

--- src/main/java/com/example/springboot/SpringbootRestfulWebservicesApplication.java
+++ src/main/java/com/example/springboot/SpringbootRestfulWebservicesApplication.java
@@ -1,17 +1,12 @@
 package com.example.springboot;
 
-import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(FileProperties.class)
 public class SpringbootRestfulWebservicesApplication {
-
-//	@Bean
-//	public ModelMapper modelMapper() {
-//		return new ModelMapper();
-//	};
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootRestfulWebservicesApplication.class, args);
